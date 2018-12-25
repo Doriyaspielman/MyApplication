196c9e4aff142542c988966bc1bcaf2915149797
noytvili <noytvili93@gmail.com>
2018-12-25 23:05:50 +0200

    the first process is done! there are 2 bugs (double V, if there is no product to buy)

--- app/src/main/java/com/example/doriyaspielman/myapplication/StoreScreen.java
+++ app/src/main/java/com/example/doriyaspielman/myapplication/StoreScreen.java
@@ -1,10 +1,16 @@
 package com.example.doriyaspielman.myapplication;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 public class StoreScreen extends AppCompatActivity {
-ListView lst;
+    private Button pay;
+
+    ListView lst;
 String[] productName = {"red shoes", "black shoes", "black coat", "casual dress", "evening dress", "pink shirt", "white shirt", "wedding dress"};
 String[] price = {"140", "120", "150", "90" , "170", "100", "80", "300"};
 Integer[] pic_id={R.drawable.red_adidas, R.drawable.black_shoes, R.drawable.black_coat, R.drawable.casual_dress, R.drawable.evening_dress, R.drawable.pink_shirt, R.drawable.white_shirt,R.drawable.wedding_dress };
@@ -16,6 +22,20 @@ Integer[] pic_id={R.drawable.red_adidas, R.drawable.black_shoes, R.drawable.blac
     lst=(ListView) findViewById(R.id.listview);
         Custom_listview custom_listview=new Custom_listview(this,productName,price,pic_id);
         lst.setAdapter(custom_listview);
+
+        this.pay = (Button) findViewById(R.id.pay);
+
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPay();
+            }
+        });
+    }
+
+    public void onPay(){
+        Intent i = new Intent(this, confirm.class);
+        startActivity(i);
 
     }
 }
